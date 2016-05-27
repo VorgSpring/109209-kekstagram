0018fbf3f0d578ff7033d55056ca4b81d811ddac
VorgSpring <drmrSP@yandex.ru>
2016-05-27 18:16:45 +0300

    Начинаем программировать #3

--- build/js/check.js
+++ build/js/check.js
@@ -2,8 +2,7 @@
     if (typeof a == 'boolean') {
         if (a) {
             return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-        }
-        else {
+        } else {
             return 'Переданное GIF-изображение не анимировано';
         }
     }
@@ -12,19 +11,25 @@
     }
     else if (Array.isArray(a)) {
         if (Array.isArray(b)) {
-            var square = null;
-            for (var i = 0; i < a.length; i++) {
-                square += a[i] * b[i];
-            }
-            return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
-        }
-        else {
-            var sum = null;
-            for (var i = 0; i < a.length; i++) {
-                sum += a[i];
-            }
-
-            return 'Количество красных точек во всех строчках изображения: ' + sum;
+            return 'Общая площадь артефактов сжатия: ' + getSquare(a, b) + ' пикселей';
+        } else {
+            return 'Количество красных точек во всех строчках изображения: ' + getSum(a);
         }
     }
+}
+
+function getSum(a) {
+    var sum = null;
+    for (var i = 0; i < a.length; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
+
+function getSquare(a, b) {
+    var square = null;
+    for (var i = 0; i < a.length; i++) {
+        square += a[i] * b[i];
+    }
+    return square;
 }
