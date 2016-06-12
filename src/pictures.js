56530cc0ad99d6f96366111a22f6858fdf6c009a
VorgSpring <drmrSP@yandex.ru>
2016-06-12 18:49:18 +0300

    written module is initialized, the list of photos

--- src/pictures.js
+++ src/pictures.js
@@ -1,5 +1,85 @@
 ﻿'use strict';
 
-// Скрываем блок с фильтрами .filters, добавляя ему класс hidden.
+/**
+ * Блок с фильтрами
+ * @type {HTMLElement}
+ */
 var filters = document.querySelector('.filters');
+// Скрываем блок с фильтрами
 filters.classList.add('hidden');
+
+// Создаём для каждой записи массива pictures блок фотографии на основе шаблона #picture-template.
+
+/**
+ * Блок с фотографиями
+ * @type {HTMLElement}
+ */
+var picturesContainer = document.querySelector('.pictures');
+
+/**
+ * Допустимое время загрузки фотографии
+ * @constant {number}
+ */
+var IMAGE_LOAD_TIMEOUT = 10000;
+
+/**
+ * Шаблон для блока с фотографиями
+ * @type {HTMLElement}
+ */
+var templateElement = document.querySelector('template');
+
+/**
+ * content элемента templateElement
+ * @type {HTMLElement}
+ */
+var elementToClone;
+
+// Если браузер не поддерживает тег 'template'
+if ('content' in templateElement) {
+  elementToClone = templateElement.content.querySelector('.picture');
+} else {
+  elementToClone = templateElement.querySelector('.picture');
+}
+
+/**
+ * @param {Object} data
+ * @param {HTMLElement} container
+ * @return {HTMLElement}
+ */
+var getPictureElement = function(data, container) {
+  // Клонируем шаблонный элемент
+  var element = elementToClone.cloneNode(true);
+  // Заполняем элемент данными о комментариях и лайках
+  element.querySelector('.picture-comments').textContent = data.comments;
+  element.querySelector('.picture-likes').textContent = data.likes;
+  // Добавляем фото
+  var contantImage = element.querySelector('img');
+  var uploadImage = new Image(182, 182);
+  var imageLoadTimeout = setTimeout(function() {
+    uploadImage.src = '';
+    contantImage.classList.add('picture-load-failure');
+  }, IMAGE_LOAD_TIMEOUT);
+
+  // Обработчик загрузки
+  uploadImage.onload = function() {
+    clearTimeout(imageLoadTimeout);
+    contantImage.src = data.url;
+  };
+
+  // Обработчик ошибки
+  uploadImage.onerror = function() {
+    contantImage.classList.add('picture-load-failure');
+  };
+
+  uploadImage.src = data.url;
+  // Вставляем элемент в DOM
+  container.appendChild(element);
+  return element;
+};
+// Перебераем список полученный с сервера
+window.pictures.forEach(function(picture) {
+  getPictureElement(picture, picturesContainer);
+});
+
+// Отображаем блок с фильтрами
+filters.classList.remove('hidden');
