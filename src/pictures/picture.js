4f1f5ecd131ab43ed30e40cc3485f25e09aa783f
VorgSpring <drmrSP@yandex.ru>
2016-07-03 08:30:25 +0300

    created by constructor Photo, poprawiny modules gallery and renderPictures

---
+++ src/pictures/picture.js
@@ -0,0 +1,41 @@
+﻿'use strict';
+
+/**
+ * Добавляет в container объект data на основе шаблона templateElement
+ * @param {Object} data
+ * @param {HTMLElement} container
+ * @return {HTMLElement} element
+ */
+var getPictureElement = require('./getPictureElement.js');
+
+
+/**
+ * Отображает галерею
+ * @param {Object} data
+ */
+var showGallery = require('../gallery.js').showGallery;
+
+
+/**
+ * Конструктор для отрисовки одной фотографии в списке
+ * @param {Object} data
+ * @param {HTMLElement} container
+ * @constructor
+ */
+var Photo = function(data, container) {
+  this.data = data;
+  this.element = getPictureElement(data, container);
+  this.onClick = function(event) {
+    event.preventDefault();
+    showGallery(data);
+  };
+  this.remove = function() {
+    this.element.removeEventListener('click', this.onClick);
+    this.element.parentNode.removeChild(this.element);
+  };
+
+  this.element.addEventListener('click', this.onClick);
+  container.appendChild(this.element);
+};
+
+module.exports = Photo;
