adcbb1aedc8bccc9c97bb58b121c3884f47202d5
VorgSpring <drmrSP@yandex.ru>
2016-07-03 08:35:00 +0300

    created by constructor Photo, poprawiny modules gallery and renderPictures (eslint)

--- src/pictures/renderPictures.js
+++ src/pictures/renderPictures.js
@@ -21,7 +21,7 @@ var renderPictures = function(images, container, page, pageSize, replace) {
   if (replace) {
     renderedPictures.forEach(function(picture) {
       picture.remove();
-    })
+    });
   }
 
   var from = page * pageSize;
